7e782d373704826d352cfd2eb16d6684a83660ef
dauriacpaul <92368381+dauriacpaul@users.noreply.github.com>
2022-06-23 15:32:17 +0200

    Update BirdCLEF.py

--- BirdCLEF.py
+++ BirdCLEF.py
@@ -152,9 +152,9 @@ if __name__ == '__main__':
         df = df.merge(labels_fr, on='sciName', how='left')
 
         if selected_bird in list(df.comName_en):
-            st.success(f"The {selected_bird} has been detected in {time_compute} seconds. This prediction consumed {emissions}gCO₂eq/kWh")
+            st.success(f"The {selected_bird} has been detected in {time_compute} seconds. This prediction consumed {emissions}gCO₂eq")
         else:
-            st.error(f"The {selected_bird} has not been detected. The computation tooked {time_compute} seconds. This prediction consumed {emissions}gCO₂eq/kWh")
+            st.error(f"The {selected_bird} has not been detected. The computation tooked {time_compute} seconds. This prediction consumed {emissions}gCO₂eq")
 
         if df.shape[0] > 0:
             df.loc[:, "url_photo"] = df.comName_en.apply(get_photo_url)
